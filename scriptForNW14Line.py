4a4edd65fb90334f1b03dc0909e688a53750c4f5
xingpan1017 <150761321+xingpan1017@users.noreply.github.com>
2024-11-29 14:44:07 -0500

    Update scriptForNW14Line.py

--- scriptForNW14Line.py
+++ scriptForNW14Line.py
@@ -5,3 +5,45 @@
 ## Restore .ms file before continuum flag
 myvis_list = ["../calibrated/cygxnw14_A002_X1096e27_X4af.ms", "../calibrated/cygxnw14_A002_X1097a87_X8203.ms"]
 
+for myvis in myvis_list:
+  flagmanager(vis=myvis, mode='restore', versionname='before_cont_flags')
+
+## Make linecube for each spw
+for i in range(4):
+    imvis = myvis_list[1]
+    imname = './spwcube/NW14_X8203_cube_spw%d'%i
+    nit = 100
+    cellsize = '0.05arcsec'
+    imsize = 100
+    robust = 0.5
+    wtg = 'briggs'
+    threshold = '0.1mJy'
+#
+    tclean(vis=imvis,
+        imagename = imname,
+        #deconvolver = 'multiscale',
+        #scales = [0,5,15,50,150],
+        deconvolver = 'hogbom',
+        imsize=imsize,
+        cell = cellsize,
+        gridder = 'standard',
+        specmode = 'cube',
+        spw = '%d'%i,
+#        #width = '5km/s',
+        stokes = 'I',
+        robust = robust,
+        weighting = wtg,
+        niter = nit,
+        interactive = False,
+        pbcor = False,
+        pblimit = 0.1,
+        usemask = 'auto-multithresh',
+        sidelobethreshold = 3.0,
+        noisethreshold = 5.0,
+        lownoisethreshold = 1.5,
+        minbeamfrac = 0.3,
+        negativethreshold = 0.0)
+
+    exportfits(imagename=imname+".image", fitsimage=imname+".image.fits", overwrite=True, history=True, dropdeg=True)
+
+
