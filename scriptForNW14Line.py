4bbf1c1223e002e3feb2c67785f189812d2aa349
xingpan1017 <150761321+xingpan1017@users.noreply.github.com>
2024-11-29 14:29:57 -0500

    Create scriptForNW14Line.py

---
+++ scriptForNW14Line.py
@@ -0,0 +1,7 @@
+#############################################################################
+## Image line emission from the two datasets
+#############################################################################
+
+## Restore .ms file before continuum flag
+myvis_list = ["../calibrated/cygxnw14_A002_X1096e27_X4af.ms", "../calibrated/cygxnw14_A002_X1097a87_X8203.ms"]
+
