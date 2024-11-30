53be6428c94074bac6b2bf75810398cf8caf16a6
xingpan1017 <150761321+xingpan1017@users.noreply.github.com>
2024-11-30 11:00:05 -0500

    Update scriptForNW14Line.py
    update some line free channels

--- scriptForNW14Line.py
+++ scriptForNW14Line.py
@@ -9,6 +9,7 @@ for myvis in myvis_list:
   flagmanager(vis=myvis, mode='restore', versionname='before_cont_flags')
 
 ## Make linecube for each spw
+myvis_list = ["../calibrated/cygxnw14_A002_X1096e27_X4af.ms", "../calibrated/cygxnw14_A002_X1097a87_X8203.ms"]
 for i in range(4):
     imvis = myvis_list[1]
     imname = './spwcube/NW14_X8203_cube_spw%d'%i
@@ -38,12 +39,20 @@ for i in range(4):
         pbcor = False,
         pblimit = 0.1,
         usemask = 'auto-multithresh',
-        sidelobethreshold = 3.0,
+        ## b75 > 400m
+        sidelobethreshold = 2.5,
         noisethreshold = 5.0,
-        lownoisethreshold = 1.5,
         minbeamfrac = 0.3,
-        negativethreshold = 0.0)
+        lownoisethreshold = 1.5,
+        negativethreshold = 0.0,
+        fastnoise = True,
+        pbmask = 0.3)
 
     exportfits(imagename=imname+".image", fitsimage=imname+".image.fits", overwrite=True, history=True, dropdeg=True)
+
+##############################################################################
+## Line-free channels
+## NW14 e1 is a line-rich source 
+fc = '0:20~45;100~150;200~350;510~600,1:165~300;370~410;500~540,'
 
 
