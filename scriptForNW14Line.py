aa40ea7c179d1422b7a64423b846d1bb83d90a2f
xingpan1017 <150761321+xingpan1017@users.noreply.github.com>
2025-02-02 17:40:32 -0500

    Update scriptForNW14Line.py

--- scriptForNW14Line.py
+++ scriptForNW14Line.py
@@ -641,7 +641,87 @@ for linevis, imname in zip(linevis_list, imname_list):
   
   exportfits(imagename=imname+".image", fitsimage=imname+".image.fits", velocity=True, overwrite=True)
 
-## Create H2CO 3(2,2)-2(2,1) directory
+import os
+import numpy as np
+molecule_list = ["H2CO_3_22_2_21", "H2CO_3_21_2_20", "13CH3OH_14_13", "13CN_2_1", \
+                 "HNCO_10_3_9_3", "H13CN_10_9", "H2CN_3_2", "CH3CHO_12_3_11_3",\
+                 "CH3OCH3_13_12", "HNO3_12_11_hf", "33SO2_4_3", "CH3OH_18_17", \
+                "CH3OH_10_11"]
+restfreq_list = ["218.4757636GHz", "218.75605262GHz", "217.044616GHz", "217.294950GHz", \
+                 "219.6567695GHz", "219.706607GHz", "219.7353481GHz", "231.9683853GHz", \
+                 "231.9879198GHz", "232.0343425GHz", "232.418353GHz", "232.783446GHz", \
+                "232.945797GHz"]
+imsize_list = [1200, 1200, 800, 800, \
+               800, 800, 800, 800, \
+               800, 800, 800, 800, \
+               800]
+niter_list = [100000, 100000, 100000, 100000, \
+              100000, 100000, 100000, 100000, \
+              100000, 100000, 100000, 100000, \
+              100000]
+spw_list = [0, 0, 1, 1, \
+            3, 3, 3, 5, \
+            5, 5, 5, 5, \
+            5]
+
+for i in np.arange(4,13):
+  molecule, restfreq, imsize, niter, spw = molecule_list[i], restfreq_list[i], imsize_list[i], niter_list[i], spw_list[i]
+  
+  if os.path.exists("./%s"%molecule):
+    os.removedirs("./%s"%molecule)
+  else:
+    os.mkdir("./%s"%molecule)
+  
+  linevis_list = ["../calibrated/cygxnw14_A002_X1096e27_X4af.ms.line", "../calibrated/cygxnw14_A002_X1097a87_X8203.ms.line"]
+  imname_list = ["./%s/cygxnw14_%s_X4af"%(molecule, molecule), "./%s/cygxnw14_%s_X8203"%(molecule, molecule)]
+
+  ## Image line data for each date
+  ## Image Parameters
+  
+  for linevis, imname in zip(linevis_list, imname_list):
+    cell = '0.015arcsec'
+    weighting = 'briggs'
+    robust = 0.5
+    threshold = '1mJy'
+    restfreq = restfreq
+    start = '-20km/s'  ## Vsys ~5.5 km/s
+    nchan = 70
+    
+    tclean(vis = linevis,
+      imagename=imname,
+      specmode='cube',
+      deconvolver = 'multiscale',
+      spw = "%d"%spw,
+      niter = niter,
+      start = start,
+      nchan = nchan,
+      scales = [0,5,15,50],
+      imsize=imsize,
+      cell=cell,
+      restfreq = restfreq,
+      threshold=threshold,  
+      gridder='standard', 
+      weighting=weighting,
+      outframe = 'LSRK', 
+      interactive = False,
+      pblimit = 0.2,
+      robust = robust,
+      usemask = 'auto-multithresh',
+    ## b75 > 400m
+      sidelobethreshold = 2.5,
+      noisethreshold = 5.0,
+      minbeamfrac = 0.3,
+      lownoisethreshold = 1.5,
+      negativethreshold = 0.0,
+      fastnoise = True)
+    
+    exportfits(imagename=imname+".image", fitsimage=imname+".image.fits", velocity=True, overwrite=True)
+    exportfits(imagename=imname+".residual", fitsimage=imname+".residual.fits", velocity=True, overwrite=True)
+    print("Finish %s image."%molecule)
+
+####################################################################################
+## Image the other two components
+
 import os
 import numpy as np
 molecule_list = ["H2CO_3_22_2_21", "H2CO_3_21_2_20", "13CH3OH_14_13", "13CN_2_1", \
